1785540d7d9fdbdadfdda968fa4afcd12f1add6e
choi-solyi <choi-solyi@naver.com>
2023-01-24 22:23:53 +0900

    #1.4 WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,16 @@
-alert('hi')
+const socket = new WebSocket(`ws://${window.location.host}`)
+
+socket.addEventListener('open', () => {
+  console.log('connected to server -- O')
+})
+
+socket.addEventListener('message', message => {
+  console.log('New message: [ ', message.data, ' ] from server')
+})
+socket.addEventListener('close', () => {
+  console.log('Disconnected to server -- X')
+})
+
+setTimeout(() => {
+  socket.send('hello from the browser')
+}, 3000)
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
 app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res) => res.render('home'))
-app.get('/*', (req, res) => res.render('/'))
+// app.get('/*', (req, res) => res.render('/'))
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
 // app.listen(3000, handleListen)
@@ -16,5 +16,16 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`)
 // http 와 ws 둘다 작동
 const server = http.createServer(app)
 const wss = new WebSocket.Server({ server })
+
+wss.on('connection', socket => {
+  console.log('Connected to browser ')
+  socket.on('close', () => {
+    console.log('Disconnect from the browser')
+  })
+  socket.on('message', message => {
+    console.log(message.toString())
+  })
+  socket.send('hello!')
+})
 
 server.listen(3000, handleListen)
